da9bed70926ca691f1c88960eb95d433b50494f6
shopie14 <sh.pinh14@gmail.com>
2023-06-07 11:51:04 +0700

    add utils app_routes

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRoutes {
+  static const home = 'home';
+  
+  static Page _homePageBuilder(BuildContext context, GoRouterState state){
+    return MaterialPage(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Home Page')),
+      )
+    )
+  }
+}
+
+
+static GoRouter goRouter = GoRouter(
+  initialLocation: '/home',
+  routes: [
+    GoRoute(
+      name: home,
+      path: '/home',
+      pageBuilder: _homePageBuilder, 
+    ),
+  ],
+);
