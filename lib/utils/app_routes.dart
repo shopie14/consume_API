2d345d5ccc6578003bd4afb0ce6b97ecc79ba801
Fadia S Nursifa <2006076@itg.ac.id>
2023-06-07 13:20:45 +0700

    add post, post service, endpoint, request

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../pages/home_page.dart';
+
 class AppRoutes {
   static const home = 'home';
-  
-  static Page _homePageBuilder(BuildContext context, GoRouterState state){
-    return MaterialPage(
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Home Page')),
-      )
-    )
+
+  static Page _homePageBuilder(BuildContext context, GoRouterState state) {
+    return const MaterialPage(
+      child: HomePage(),
+    );
   }
+
+  static GoRouter goRouter = GoRouter(
+    initialLocation: '/home',
+    routes: [
+      GoRoute(
+        name: home,
+        path: '/home',
+        pageBuilder: _homePageBuilder,
+      ),
+    ],
+  );
 }
-
-
-static GoRouter goRouter = GoRouter(
-  initialLocation: '/home',
-  routes: [
-    GoRoute(
-      name: home,
-      path: '/home',
-      pageBuilder: _homePageBuilder, 
-    ),
-  ],
-);
---
+++ lib/utils/endpoint.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+
+class EndPoint {
+  static const _baseUrl = "https://jsonplaceholder.typicode.com";
+  static const post = "$_baseUrl/posts";
+  static const comment = "$_baseUrl/comment";
+  static const album = "$_baseUrl/albums";
+  static const photo = "$_baseUrl/photos";
+  static const todo = "$_baseUrl/todos";
+  static const user = "$_baseUrl/users";
+}
