f41802f7c67c891baeed15cbdb3b1c7ad2db5ec7
shopie14 <sh.pinh14@gmail.com>
2023-06-09 08:52:30 +0700

    update home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,82 +1,43 @@
 import 'package:flutter/material.dart';
 import '../controllers/post_controllers.dart';
+import '../models/post.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class HomePageState extends State<HomePage> { 
-    final PostController postController = PostController()
-    @override
-    Widget build(BuildContext context) {
-      final Size size = MediaQuery.of(context).size;
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text("Home Page"),
-          ), // AppBar
-          body: SafeArea(
-            child: FutureBuilder<List<Post>>(
-               builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasData) {
-            if (snapshot.data!.isNotEmpty) {
-              inspect(snapshot.data!);
-              return Padding(
-                padding:EdgeInsets.symmetric(horizontal: size.width * 0.01),
-                child: ListView.separated(itemBuilder:(context, index){
-                  return Dismissible(
-                    key: Key(snapshot.data![index].id.toString()),
-                    onDismissed: (direction) {
-                      postController.delete(snapshot.data![index].id).then((result){
-                        if (result){
-                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("post delete"),
-                          behavior: SnackBarBehavior.floating,
-                          ),
-                          );
-                          setState(() {
-                            
-                          });
-                        }else{
-                           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("failed to delete post"),
-                           behavior: SnackBarBehavior.floating,
-                           ),
-                           );
-                           setState(() {
-                             
-                           });
-                        }
-
-                      });
-                    },
-                    child: Card(
-                      child: ListTile(
-                        title: Text(snapshot.data![index].title,maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        ),
-                        subtitle: Text(snapshot.data![index].body, maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ),
-                  );
-              
-                },    
-                  
-                           }, separatorBuilder: (context, index) {
-                return SizedBox(height: size.height * 0.0005,
-                );
-                           }, itemCount: snapshot.data!.length,
-                           ),
+class _HomePageState extends State<HomePage> {
+  final PostController postController = PostController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: SafeArea(
+        child: FutureBuilder<List<Post>>(
+          future: postController.fetchAll(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
               );
-          } else {
-              return Text("Tidak ada data")
-            }
+            } else if (snapshot.hasData) {
+              if (snapshot.data!.isNotEmpty) {
+                inspect(snapshot.data!);
+                return Text("Ada Data");
+              } else {
+                return Text("Tidak Ada Data");
+              }
             } else {
-              return Text ("error");
+              return Text("Error");
             }
+          },
+        ),
+      ),
+    );
+  }
+}
